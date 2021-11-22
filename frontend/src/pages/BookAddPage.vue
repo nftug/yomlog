5d87f7ea4b69db3de0b11766baba2e0b196ae07d
nftug <whitepony1214@gmail.com>
2021-11-22 12:50:49 +0900

    書籍追加: ページネーションを実装

--- frontend/src/pages/BookAddPage.vue
+++ frontend/src/pages/BookAddPage.vue
@@ -6,14 +6,11 @@
         label="検索キーワード"
         prepend-icon="mdi-magnify"
         clearable
-        @keydown.enter="doSearch()"
+        @keydown.enter="this.changeQuery('q', searchValue, true)"
       ></v-text-field>
 
-      <v-subheader>
-        Google Booksからの検索結果 ({{ totalForGoogle }}件)
-      </v-subheader>
       <v-row>
-        <v-col v-for="item in itemsForGoogle" :key="item.id" cols="12" lg="6">
+        <v-col v-for="item in items" :key="item.id" cols="12" lg="6">
           <v-card class="mx-auto" height="185">
             <v-row no-gutters>
               <v-col cols="8">
@@ -44,45 +41,104 @@
           </v-card>
         </v-col>
       </v-row>
+
+      <infinite-loading @infinite="infiniteHandler" :identifier="infiniteId">
+        <div slot="no-more" class="py-4 text-body-2">
+          これ以上通知はありません
+        </div>
+        <div slot="no-results" class="py-4 text-body-2">
+          データが見つかりません
+        </div>
+        <div slot="spinner" class="py-4">
+          <spinner />
+        </div>
+      </infinite-loading>
     </div>
   </v-container>
 </template>
 
 <script>
 import axios from 'axios'
+import Spinner from 'vue-simple-spinner'
+import InfiniteLoading from 'vue-infinite-loading'
 
 export default {
+  components: {
+    Spinner,
+    InfiniteLoading,
+  },
   data: () => ({
     noImageCover:
       'https://dummyimage.com/140x185/c4c4c4/636363.png&text=NoImage',
     searchValue: '',
-    itemsForGoogle: [],
-    totalForGoogle: 0,
-    pageForGoogle: 1,
+    items: [],
+    total: 0,
+    page: 0,
+    maxResults: 12,
     infiniteId: +new Date(),
   }),
+  beforeRouteUpdate(to, from, next) {
+    // URLパラメータから値を設定してfetchする
+    this.fetchBookList(to.query)
+    next()
+  },
   methods: {
-    doSearch() {
-      // TODO: 無限スクロールに対応させる
-      if (this.searchValue) {
-        // TODO: クリア処理を無限スクロール関連のメソッドに移す
-        this.itemsForGoogle = []
+    fetchBookList(query) {
+      // BUG: Google Books APIのtotalItemsの数はあてにならない (非固定)
+      // →ページネーションの割り振りには使えない
 
-        axios
+      this.searchValue = query.q || ''
+
+      if (this.searchValue) {
+        const startIndex = (this.page - 1) * (this.maxResults + 1)
+
+        return axios
           .get('https://www.googleapis.com/books/v1/volumes', {
             params: {
               q: this.searchValue,
               orderBy: 'relevance',
+              maxResults: this.maxResults,
+              startIndex: startIndex,
             },
           })
           .then(({ data }) => {
-            this.totalForGoogle = data.totalItems
+            this.total = data.totalItems
             data.items.forEach((item) => {
               item.volumeInfo.authors = item.volumeInfo.authors || ['不明']
-              this.itemsForGoogle.push(item.volumeInfo)
+              this.items.push(item.volumeInfo)
             })
+
+            return Promise.resolve()
+          })
+          .catch((error) => {
+            return Promise.reject(error)
           })
       }
+    },
+    changeQuery(key, val, isClear = false) {
+      const query = isClear ? {} : Object.assign({}, this.$route.query)
+      query[key] = val
+
+      this.items = []
+
+      if (JSON.stringify(query) != JSON.stringify(this.$route.query)) {
+        this.$router.push({
+          path: this.$route.path,
+          query: query,
+        })
+      } else {
+        this.fetchBookList(this.$route.query)
+      }
+    },
+    infiniteHandler($state) {
+      this.page++
+      this.fetchBookList(this.$route.query)
+        .then(() => {
+          $state.loaded()
+        })
+        .catch(() => {
+          $state.complete()
+        })
     },
   },
 }
