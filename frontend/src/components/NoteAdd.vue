b8447aa34fd22ba2638b8efd1553e1ff4b0b16ad
nftug <whitepony1214@gmail.com>
2021-12-06 10:03:22 +0900

    NoteAdd: UIを修正

--- frontend/src/components/NoteAdd.vue
+++ frontend/src/components/NoteAdd.vue
@@ -7,44 +7,73 @@
     transition="dialog-bottom-transition"
     :form-valid="isValid"
   >
+    <template #toolbar="{ ok, cancel, title }">
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click="cancel">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>{{ title }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn dark icon @click="ok" :disabled="!isValid">
+            <v-icon>mdi-content-save</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+
+        <template #extension>
+          <v-tabs v-model="tab" centered>
+            <v-tab>ノート</v-tab>
+            <v-tab>引用の詳細</v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+    </template>
+
     <spinner size="large" v-if="isSending" />
 
     <v-form v-else ref="formNoteAdd" v-model="isValid">
-      <v-col cols="10" lg="5" md="6" sm="10" class="mx-auto mt-5">
-        <v-text-field
-          v-model="position"
-          :label="!format_type ? 'ページ数' : '位置No'"
-          type="number"
-          min="0"
-          :max="total"
-          :suffix="` / ${total}`"
-          :rules="positionRules"
-        ></v-text-field>
+      <v-col cols="11" lg="5" md="6" sm="10" class="mx-auto mt-5">
+        <v-tabs-items v-model="tab">
+          <v-tab-item class="mt-3">
+            <v-text-field
+              v-model="position"
+              :label="!format_type ? 'ページ数' : '位置No'"
+              type="number"
+              min="0"
+              :max="total"
+              :suffix="` / ${total}`"
+              :rules="positionRules"
+            ></v-text-field>
 
-        <v-textarea
-          v-model="quoteText"
-          outlined
-          label="引用内容 (テキスト)"
-        ></v-textarea>
+            <v-textarea
+              v-model="content"
+              outlined
+              label="ノートの内容"
+              rows="8"
+              :rules="contentRules"
+            ></v-textarea>
+          </v-tab-item>
 
-        <v-file-input
-          v-model="quoteImage"
-          label="引用内容 (画像)"
-          ref="quote_image"
-          accept="image/*"
-          @change="inputQuoteImage($event)"
-        ></v-file-input>
+          <v-tab-item class="mt-3">
+            <v-textarea
+              v-model="quoteText"
+              outlined
+              label="引用内容 (テキスト)"
+            ></v-textarea>
 
-        <div v-show="prevSrc" class="mb-4 mx-4">
-          <v-img :src="prevSrc" alt="" width="150" />
-        </div>
+            <v-file-input
+              v-model="quoteImage"
+              label="引用内容 (画像)"
+              ref="quote_image"
+              accept="image/*"
+              @change="inputQuoteImage($event)"
+            ></v-file-input>
 
-        <v-textarea
-          v-model="content"
-          outlined
-          label="ノートの内容"
-          :rules="contentRules"
-        ></v-textarea>
+            <div v-show="prevSrc" class="mb-4 mx-4">
+              <v-img :src="prevSrc" alt="" width="150" />
+            </div>
+          </v-tab-item>
+        </v-tabs-items>
       </v-col>
     </v-form>
   </Dialog>
@@ -85,6 +114,7 @@ export default {
       quoteImage: null,
       prevSrc: '',
       isSending: false,
+      tab: null,
     }
   },
   methods: {
