bdce1b7c9e660986489a693f5d638af589ad5464
nftug <whitepony1214@gmail.com>
2021-12-10 12:59:22 +0900

    fix some

--- frontend/src/components/StatusLog.vue
+++ frontend/src/components/StatusLog.vue
@@ -10,23 +10,41 @@
         <v-row class="col-lg-8 mx-auto">
           <v-list-item class="py-3">
             <v-row align="center">
-              <v-col class="flex-grow-1 flex-shrink-0">
+              <v-list-item-content>
                 <div class="text-lg-h5 text-h6">
                   {{ state.position }} / {{ item.total }}
                   <span class="text-body-2">
                     {{ item.format_type ? '' : 'ページ' }}
                   </span>
                 </div>
-              </v-col>
+              </v-list-item-content>
 
-              <v-col cols="5" class="flex-grow-0 flex-shrink-0" align="right">
+              <v-list-item-action>
                 <div class="text-body-2">
                   {{ state.created_at | isoToDateTime }}
                 </div>
                 <v-chip small class="mt-2" :color="state.state | stateColor">
                   {{ state.state | stateName }}
                 </v-chip>
-              </v-col>
+              </v-list-item-action>
+
+              <v-list-item-action>
+                <v-menu offset-y>
+                  <template #activator="{ on, attrs }">
+                    <v-btn small icon v-bind="attrs" v-on="on">
+                      <v-icon small>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                  </template>
+                  <v-list dense close-on-click>
+                    <v-list-item link>
+                      <v-list-item-title>編集</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item link>
+                      <v-list-item-title>削除</v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-list-item-action>
             </v-row>
           </v-list-item>
         </v-row>
