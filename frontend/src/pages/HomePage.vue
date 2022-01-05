46613f61bb43287c4318b7e0bf16a897ed820051
nftug <whitepony1214@gmail.com>
2022-01-05 13:10:01 +0900

    HomePage: 実装中

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -22,39 +22,48 @@
         </v-list>
       </v-card>
 
-      <!-- 読書データ -->
+      <!-- 分析 -->
       <v-card outlined class="text-center">
         <v-row class="mx-auto my-3">
-          <v-col cols="6" md="3">
-            <div class="text-body-2">読んだ本</div>
-            <div class="text-h5 mt-1">
-              {{ numOfBooks.read }}
-              <span class="text-body-2">冊</span>
-            </div>
-          </v-col>
-          <v-col cols="6" md="3">
-            <div class="text-body-2">読んだページ数</div>
-            <div class="text-h5 mt-1">
-              {{ pages.total }}
-              <span class="text-body-2">ページ</span>
-            </div>
-          </v-col>
-          <v-col cols="6" md="3">
-            <div class="text-body-2">一日の平均</div>
-            <div class="text-h5 mt-1">
-              {{ pages.avg_per_day }}
-              <span class="text-body-2">ページ</span>
-            </div>
-          </v-col>
-          <v-col cols="6" md="3">
-            <div class="text-body-2">連続読書日数</div>
-            <div class="text-h5 mt-1">
-              {{ days.continuous }}
-              <span class="text-body-2">日</span>
-            </div>
-          </v-col>
+          <template v-for="(item, index) in analytics">
+            <v-col cols="6" md="3" :key="index">
+              <div class="text-body-2" v-text="item.title"></div>
+              <div class="text-h5 mt-1">
+                {{ item.value }}
+                <span class="text-body-2">{{ item.unit }}</span>
+              </div>
+            </v-col>
+          </template>
         </v-row>
       </v-card>
+
+      <!-- 読書データ -->
+      <v-row class="my-3">
+        <v-col cols="12" md="6">
+          <v-card outlined>
+            <v-card-title class="mx-3 mt-3">読書データ</v-card-title>
+            <v-card-text>
+              <v-list>
+                <template v-for="(item, index) in bookAnalytics">
+                  <div :key="index">
+                    <v-list-item :to="item.to">
+                      <v-list-item-icon>
+                        <v-icon v-text="item.icon" :color="item.color"></v-icon>
+                      </v-list-item-icon>
+                      {{ item.title }}
+                      <v-spacer></v-spacer>
+                      <strong>{{ item.value }} 冊</strong>
+                    </v-list-item>
+                    <v-divider
+                      v-if="index + 1 < bookAnalytics.length"
+                    ></v-divider>
+                  </div>
+                </template>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
   </v-container>
 </template>
@@ -65,6 +74,10 @@ import { mapGetters, mapState } from 'vuex'
 
 export default {
   mixins: [Mixin],
+  data: () => ({
+    analytics: [],
+    bookAnalytics: [],
+  }),
   computed: {
     ...mapState(['auth']),
     ...mapGetters({ created_at: 'auth/created_at' }),
@@ -73,6 +86,53 @@ export default {
       pages: (state) => state.auth.analytics.pages_read,
       days: (state) => state.auth.analytics.days,
     }),
+  },
+  created() {
+    this.analytics = [
+      {
+        title: '読んだ本',
+        value: this.numOfBooks.read,
+        unit: '冊',
+      },
+      {
+        title: '読んだページ数',
+        value: this.pages.total,
+        unit: 'ページ',
+      },
+      {
+        title: '一日の平均',
+        value: this.pages.avg_per_day,
+        unit: 'ページ',
+      },
+      {
+        title: '連続読書日数',
+        value: this.days.continuous,
+        unit: '日',
+      },
+    ]
+    this.bookAnalytics = [
+      {
+        title: '読んだ本',
+        icon: 'mdi-book-check',
+        color: 'green',
+        value: this.numOfBooks.read,
+        to: '/shelf/read',
+      },
+      {
+        title: '読んでいる本',
+        icon: 'mdi-book-open-variant',
+        color: 'blue',
+        value: this.numOfBooks.reading,
+        to: '/shelf/reading',
+      },
+      {
+        title: 'あとで読む',
+        icon: 'mdi-book-clock',
+        color: 'orange',
+        value: this.numOfBooks.to_be_read,
+        to: '/shelf/to_be_read',
+      },
+    ]
   },
 }
 </script>
