055d28383ddf260b9d3ea9cacd5b07382ed4e3f4
nftug <whitepony1214@gmail.com>
2022-02-25 17:10:53 +0900

    ランディングページ: 実装

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,25 +1,20 @@
 <template>
   <v-container fluid>
-    <v-parallax
-      dark
-      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    >
+    <v-parallax dark src="@/assets/jumbotron_bg.jpg">
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
           <h1 class="text-h3 font-weight-thin mb-4">{{ appName }}</h1>
-          <h4 class="subheading mt-7">
+          <div class="text-body-2 mt-7">
             <div class="mb-1">
-              {{
-                appName
-              }}
+              {{ appName }}
               はあなたの読書ライフをサポートする読書管理サービスです。
             </div>
             <div class="mb-1">
               日々の読書状況を可視化して、読書の習慣を継続させましょう。
             </div>
-          </h4>
+          </div>
           <v-btn class="cyan darken-1 mt-7" dark x-large to="/login">
-            使ってみる
+            <strong>使ってみる</strong>
           </v-btn>
         </v-col>
       </v-row>
@@ -63,6 +58,50 @@
           <br />
           自動で対応する紙の本のページ番号に変換します。
         </IntroductionCard>
+      </v-row>
+
+      <v-divider class="my-8"></v-divider>
+
+      <v-row align="center" justify="center">
+        <v-col cols="12" md="8" lg="6">
+          <v-card class="elevation-0 transparent my-2">
+            <v-card-title class="headline justify-center">
+              {{ appName }}をはじめよう
+            </v-card-title>
+            <v-card-text class="text-center text-body-2">
+              {{ appName }}の利用はこちらからどうぞ。
+            </v-card-text>
+
+            <v-card-actions class="justify-center mt-3">
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-btn
+                    class="cyan darken-1 col-12 col-sm-6"
+                    dark
+                    x-large
+                    to="/login"
+                    block
+                  >
+                    <v-icon left size="24">mdi-login</v-icon>
+                    ログイン
+                  </v-btn>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-btn
+                    class="indigo darken-1 col-12 col-sm-6"
+                    dark
+                    x-large
+                    to="/signup"
+                    block
+                  >
+                    <v-icon left size="24">mdi-account-plus</v-icon>
+                    ユーザー登録
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+        </v-col>
       </v-row>
     </v-col>
   </v-container>
